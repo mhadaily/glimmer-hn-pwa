d64e3776ba3e054e806c4ed3c4c0af31644867b8
Majid <mhadaily@gmail.com>
2017-06-25 23:38:00 +0200

    fetch data based on route

--- src/ui/components/glimmer-hn-pwa/component.ts
+++ src/ui/components/glimmer-hn-pwa/component.ts
@@ -1,20 +1,73 @@
 import Component, { tracked } from '@glimmer/component';
+import Navigo from 'navigo';
+import fetchItems from '../../../utils/fetch';
+const API = 'https://node-hnapi.herokuapp.com';
+
+const router = new Navigo(null, true);
 
 export default class GlimmerHnPwa extends Component {
-  api = 'https://node-hnapi.herokuapp.com';
-  endpoint = this.api+'/newest?page=1';
 
-  @tracked
-  results: any[];
+  @tracked results: any[];
+  @tracked routeMode: string = 'top';
 
   constructor(options) {
     super(options);
-    this.loadNews();
+    router
+      .on({
+        '/': () => this.topNews(),
+        '/new': () => this.new(),
+        '/show': () => this.show(),
+        '/ask': () => this.ask(),
+        '/jobs': () => this.jobs(),
+        '/user/:username': (username) => this.user(username),
+        '/item/:id': (id) => this.comment(id),
+      })
+      .resolve();
   }
 
-  async loadNews() {
-    const request = await fetch(this.endpoint);
-    const json = await request.json();
-    this.results = json;
+  getEndpoint(model, page?) {
+    return page ? `${API}/${model}?page=${page}` : `${API}/${model}`;
+  }
+
+  topNews() {
+    this.routeMode = 'top';
+    this.loadModel(this.getEndpoint('news', 1));
+  }
+
+  new() {
+    this.routeMode = 'new';
+    this.loadModel(this.getEndpoint('newest', 1));
+  }
+
+  show() {
+    this.routeMode = 'show';
+    this.loadModel(this.getEndpoint('show', 1));
+  }
+
+  ask() {
+    this.routeMode = 'ask';
+    this.loadModel(this.getEndpoint('ask', 1));
+  }
+
+  jobs() {
+    this.routeMode = 'jobs';
+    this.loadModel(this.getEndpoint('jobs', 1));
+  }
+
+  user({ username }) {
+    this.routeMode = 'user';
+    this.loadModel(this.getEndpoint(`user/${username}`));
+  }
+
+  comment({ id }) {
+    this.routeMode = 'comment';
+    this.loadModel(this.getEndpoint(`item/${id}`));
+  }
+
+  loadModel(endpoint) {
+    this.results = [];
+    fetchItems(endpoint).then((res) => {
+      this.results = [ ...res ];
+    });
   }
 }
