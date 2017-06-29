7067af28ff86bcb74d8c425dd7d0560cdffa378c
Majid <mhadaily@gmail.com>
2017-06-29 21:13:32 +0200

    news model

---
+++ src/utils/model/news.ts
@@ -0,0 +1,12 @@
+export interface news {
+  id: number;
+  comments_count: number;
+  domain: string;
+  points: number;
+  time: number;
+  time_ago: string;
+  title: string;
+  type: string;
+  url: string;
+  user: string;
+}
