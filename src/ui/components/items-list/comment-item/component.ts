cccd12caafffb2f35770a0a3fded9c6aec1df18d
Majid <mhadaily@gmail.com>
2017-06-29 23:37:14 +0200

    delete components which are not used

--- src/ui/components/items-list/comment-item/component.ts
+++
@@ -1,7 +0,0 @@
-import Component, { tracked } from '@glimmer/component';
-
-export default class CommentItem extends Component {
-  constructor(args) {
-    super(args);
-  }
-}
--- src/ui/components/items-list/component.ts
+++
@@ -1,4 +0,0 @@
-import Component, { tracked } from '@glimmer/component';
-
-export default class ItemsList extends Component {
-}
